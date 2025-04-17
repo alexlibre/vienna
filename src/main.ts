b701436d0b2e67ef18cd746f548fd7b964d396db
Alexander Poskorkov <alexeration@gmail.com>
2025-04-17 10:57:56 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import { router } from "./router/router";
+import App from "@/App.vue";
+import { router } from "@/router/router";
 
-import "./assets/styles/main.css";
+import "@/assets/styles/main.css";
 
 import { createPinia } from "pinia";
 
