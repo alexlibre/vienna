08e330ec191f887fa335c1c49bbcc2ce767810f0
Alexander Poskorkov <alexeration@gmail.com>
2025-04-17 10:40:08 +0300

    chore: updates

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-// import router from "./router";
+import router from "./router";
 
 import "./assets/styles/main.css";
 
@@ -9,7 +9,7 @@ import { createPinia } from "pinia";
 const pinia = createPinia();
 
 const app = createApp(App);
-// app.use(router);
+app.use(router);
 app.use(pinia);
 
 app.mount("#app");
