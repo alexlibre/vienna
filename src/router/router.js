b1a5af70dabd5454f584dfc44be36b98e9b68825
Alexander Poskorkov <alexeration@gmail.com>
2025-04-17 10:59:54 +0300

    update

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,7 @@
-import { type RouteRecordRaw } from "vue-router";
+// import { type RouteRecordRaw } from "vue-router";
 import { createRouter, createWebHistory } from "vue-router";
 
-const routes: Array<RouteRecordRaw> = [
+const routes = [
   {
     path: "/",
     name: "home",
